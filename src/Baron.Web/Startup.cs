f77b6ea2ac4fdc2d8247529fc49654f73e802938
cagricomen <30835470+cagricomen@users.noreply.github.com>
2021-05-11 00:42:32 +0300

    change database

--- src/Baron.Web/Startup.cs
+++ src/Baron.Web/Startup.cs
@@ -24,7 +24,7 @@ namespace Baron.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<BContext>(
-                options => options.UseNpgsql("Server=localhost;Port=5432;Database=Baron;User Id=postgres;Password=admin;")
+                options => options.UseNpgsql("Server=localhost;Port=5432;Database=Monitoring;User Id=postgres;Password=admin;")
             );
 
               services
